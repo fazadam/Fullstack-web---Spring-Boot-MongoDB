1a3c15ef205e48e03cc3475ad454dbc4b6f13920
fazadam <fazadam@hotmail.com>
2023-02-21 20:35:57 +0100

    Revert "testingLoginAndSignup"
    This reverts commit 22497fc64c5908e542876329c8bf02f2926d531e.

--- springMongoDBLogin/src/main/java/com/springMongoDBLogin/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springMongoDBLogin.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.springMongoDBLogin.domain.User;
-
-public interface UserRepository extends MongoRepository<User, String>{
-	User findByUsername(String username);
-}
